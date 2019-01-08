e839c6c7df7ce5a03d95c2ae298fe11f0a901c4e
coder-ysj <jailjt.editor@gmail.com>
2019-01-08 16:09:20 +0800

    add: 项目类型

---
+++ assets/entity.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    value: 'podcast',
+    title: '播客'
+  },
+  {
+    value: 'tvSeason',
+    title: '电视剧'
+  },
+  {
+    value: 'movie',
+    title: '电影'
+  },
+  {
+    value: 'ebook',
+    title: '电子书'
+  },
+  {
+    value: 'shortFilm',
+    title: '短片'
+  },
+  {
+    value: 'album',
+    title: '音乐'
+  },
+  {
+    value: 'musicVideo',
+    title: 'MV'
+  },
+  {
+    value: 'software',
+    title: '应用程序'
+  },
+  {
+    value: 'audiobook',
+    title: '有声读物'
+  }
+]
