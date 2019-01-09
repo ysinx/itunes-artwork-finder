652ba454542af3f9b848bcfd9d48528d11d4d8c7
coder-ysj <jailjt.editor@gmail.com>
2019-01-09 09:01:19 +0800

    add: 新增空白 UI，去除旧文件

---
+++ components/empty.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="empty"></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+<style scoped>
+div.empty {
+  display: block;
+  height: 100vh;
+  width: 100%;
+  background: url('../static/filesearch.svg') center no-repeat;
+  background-size: 100px;
+}
+
+@media screen and (max-width: 768px) {
+  div.empty {
+    height: calc(100vh - 60px);
+  }
+}
+</style>
