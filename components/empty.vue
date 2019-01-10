ec59b8b11948fd627c0d608066f999b49be93e31
coder-ysj <jailjt.editor@gmail.com>
2019-01-10 09:39:55 +0800

    update: 空白页新增空数据图标

--- components/empty.vue
+++ components/empty.vue
@@ -1,19 +1,31 @@
 <template>
-  <div class="empty"></div>
+  <div class="empty">
+    <i class="empty" v-if="!itunesResult"></i>
+    <i class="empty zero" v-else-if="itunesResult && itunesResult.length <= 0"></i>
+  </div>
 </template>
-
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+export default Vue.extend({
+  props: ['itunesResult']
+})
 </script>
-
 <style scoped>
 div.empty {
   display: block;
   height: 100vh;
   width: 100%;
+}
+
+i.empty {
+  display: block;
+  height: 100%;
+  width: 100%;
   background: url('../static/filesearch.svg') center no-repeat;
   background-size: 100px;
+}
+i.empty.zero {
+  background-image: url("../static/frown.svg")
 }
 
 @media screen and (max-width: 768px) {
