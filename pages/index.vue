58639599c20d249b869038b94840bc4e355a1d5f
coder-ysj <jailjt.editor@gmail.com>
2019-01-26 21:26:12 +0800

    fixed: 首页重构

--- pages/index.vue
+++ pages/index.vue
@@ -1,129 +1,196 @@
 <template>
   <div id="home">
-    <div id="home-header">
-      <i class="sidebar-toggle" @click.self.stop="toggleSidebar(true)"></i>
-    </div>
-    <Empty v-if="!itunesResult || itunesResult.length <= 0" :itunesResult="itunesResult"/>
-    <div
-      id="home-card"
-      :class="{ lock: toggleStatus || !itunesResult || itunesResult.length <= 0 }"
-    >
-      <Card
-        v-for="item in itunesResult"
-        :key="item.id"
-        :src="item"
-        :selectedCard="selectedCard"
-        v-on:selectCard="selectCard($event)"
+    <div id="side">
+      <TextSelect title="类别" :data="entity" v-model="project.entity"/>
+      <TextSelect title="国家 / 地区" :data="country" v-model="project.country"/>
+      <TextInput
+        title="关键字"
+        placeholder="例如：Daft Punk"
+        v-model="project.name"
+        @keyup.enter.native="search()"
       />
+      <button class="confirm" v-if="!isGettingData" @click.self.stop="search()">搜索</button>
+      <button class="confirm" v-else disabled>
+        <i></i>
+      </button>
+      <footer>
+        Dev by coder-ysj.
+        <br>Already open source on
+        <a
+          href="https://github.com/coder-ysj/jayyan.net-itunes"
+          target="_blank"
+          rel="noopener"
+        >GitHub</a>.
+        <br>Copyright © 2019 coder-ysj.
+      </footer>
     </div>
-    <Download :selectedCard="selectedCard" v-on:clearSelectedCard="selectedCard = []"/>
-    <SideBar
-      :toggleStatus="toggleStatus"
-      :itunesResult="itunesResult"
-      v-on:toggleSidebar="toggleSidebar($event)"
-      v-on:clearSelectedCard="selectedCard = []"
-      v-on:itunesResultCallback="itunesResult = $event; toggleSidebar(false);"
-    />
   </div>
 </template>
 <script>
-import Vue from 'vue'
+import axios from 'axios'
 
-import SideBar from '../components/sidebar.vue'
-import Empty from '../components/empty.vue'
-import Card from '../components/card.vue'
-import Download from '../components/download.vue'
+import entityJson from '../assets/entity.json'
+import countryJson from '../assets/country.json'
+
+import TextSelect from '../components/textSelect.vue'
+import TextInput from '../components/textInput.vue'
 
 export default {
   components: {
-    SideBar,
-    Empty,
-    Card,
-    Download
+    TextSelect,
+    TextInput
   },
   data() {
     return {
-      toggleStatus: true,
-      itunesResult: null,
-      selectedCard: []
+      entity: entityJson,
+      country: countryJson,
+      isGettingData: false,
+      project: {
+        name: null,
+        entity: '- 请选择 -',
+        country: '- 请选择 -'
+      }
     }
   },
-  created() {
-    window.addEventListener('resize', () => {
-      if (window.innerWidth >= 768) {
-        this.toggleStatus = false
-      }
-    })
+  // 检索历史搜索数据是否合法
+  mounted() {
+    let project
+
+    try {
+      project = JSON.parse(localStorage['history'])
+    } catch {
+      return
+    }
+
+    if (!this.verifyKey(entityJson, project.entity)) return
+    if (!this.verifyKey(countryJson, project.country)) return
+    this.project = project
   },
   methods: {
-    toggleSidebar(statusCode) {
-      this.toggleStatus = statusCode
-    },
-    selectCard(data) {
-      if (this.selectedCard.length <= 0) {
-        this.selectedCard.push(data)
-      } else {
-        for (let index = 0; index < this.selectedCard.length; index++) {
-          if (Object.is(this.selectedCard[index], data)) {
-            this.selectedCard.splice(index, 1)
+    search() {
+      // 表单检验
+      if (!this.project.name) {
+        alert('请输入关键字')
+        return
+      } else if (this.project.entity === '- 请选择 -') {
+        alert('请选择类别')
+        return
+      } else if (this.project.country === '- 请选择 -') {
+        alert('请选择国家 / 地区')
+        return
+      }
+
+      // 刷新 UI
+      this.isGettingData = true
+
+      // 存储搜索对象
+      localStorage.setItem('history', JSON.stringify(this.project))
+
+      // 获取 iTunes 数据
+      axios
+        .get('https://api.jayyan.net/itunes/list', {
+          params: {
+            name: this.project.name,
+            country: this.project.country,
+            entity: this.project.entity,
+            limit: 50
+          }
+        })
+        .then(response => {
+          const res = response.data
+          if (!res.results || res.results.length <= 0) {
+            this.$emit('itunesResultCallback', [])
+            this.isGettingData = false
             return
           }
-        }
-        this.selectedCard.push(data)
-      }
+          this.isGettingData = false
+          this.$emit('itunesResultCallback', res.results)
+        })
+        .catch(() => {
+          this.$emit('itunesResultCallback', [])
+          this.isGettingData = false
+        })
+    },
+    verifyKey(arr, key) {
+      const json = arr[0]
+      if (json[key] !== undefined) return true
+      return false
     }
   }
 }
 </script>
 <style>
 div#home {
-  padding-left: 350px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100vh;
+  width: 100%;
+  min-width: 320px;
+  padding: 30px;
+  color: rgb(198, 208, 235);
+  background-color: rgba(4, 8, 37, 1);
 }
 
-div#home-header {
-  display: none;
+div#side {
+  display: block;
   width: 100%;
-  padding: 10px 15px;
-  text-align: left;
+  max-width: 400px;
   overflow: hidden;
 }
 
-i.sidebar-toggle {
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+button.confirm {
   cursor: pointer;
   display: block;
-  height: 40px;
-  width: 40px;
-  background: url('../static/search.svg') center no-repeat;
-  background-size: 30px;
+  margin-top: 30px;
+  width: 100%;
+  border: 0;
+  border-radius: 6px;
+  padding: 5px 10px;
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 2;
+  appearance: none;
+  outline: 0;
+  color: #fff;
+  background: linear-gradient(
+    104.74deg,
+    rgb(176, 30, 255) 0%,
+    rgb(225, 70, 124) 100%
+  );
+  overflow: hidden;
+}
+button.confirm > i {
+  display: block;
+  height: 32px;
+  width: 32px;
+  margin: auto;
+  background: url('../static/loading.svg') center no-repeat;
+  background-size: 22px;
+  animation: spin 1s infinite linear;
 }
 
-div#home-card {
+footer {
   display: block;
   width: 100%;
-  padding: 30px 15px;
-  font-size: 0;
-  text-align: center;
-}
-div#home-card.lock {
-  position: fixed;
-  top: 60px;
-  right: 0;
-  bottom: 0;
-  left: 0;
+  margin-top: 30px;
+  font-size: 12px;
+  line-height: 1.6;
+  color: rgb(198, 208, 235);
+  text-align: left;
 }
 
-@media screen and (max-width: 768px) {
-  div#home {
-    padding: 0;
-  }
-
-  div#home-header {
-    display: block;
-  }
-}
-@media screen and (max-width: 600px) {
-  div#home-card {
-    padding: 0 15px 15px 15px;
-  }
+a {
+  font-weight: 500;
+  color: rgb(156, 114, 248);
 }
 </style>
