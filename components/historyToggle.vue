555e84752bafd5bbeb550ce530c0f07f2b829fd4
coder-ysj <jailjt.editor@gmail.com>
2019-02-06 14:53:56 +0800

    update: 历史记录开关组件完成

--- components/historyToggle.vue
+++ components/historyToggle.vue
@@ -1,22 +1,29 @@
 <template>
-  <div class="toggle">
+  <div class="toggle" :class="{ dark: store_theme === 1 }">
     <label for="toggle">
+      <input type="checkbox" v-model="store_history" :class="{ selected: store_history }">
       <span>{{ title }}</span>
-      <input type="checkbox" v-model="isHistoryEnabled" :class="{ selected: isHistoryEnabled }">
     </label>
   </div>
 </template>
 <script>
+import { mapState } from 'vuex'
+
 export default {
-  props: ['title', 'status'],
-  data() {
-    return {
-      isHistoryEnabled: this.$props.status
-    }
-  },
-  watch: {
-    status(val) {
-      this.isHistoryEnabled = val
+  props: ['title'],
+  computed: {
+    store_theme: {
+      get() {
+        return this.$store.state.theme
+      }
+    },
+    store_history: {
+      get() {
+        return this.$store.state.history
+      },
+      set() {
+        this.$store.commit('changeHistory', !this.$store.state.history)
+      }
     }
   }
 }
@@ -24,47 +31,62 @@ export default {
 <style scoped>
 div.toggle {
   display: block;
-  float: right;
-  margin: 20px 0;
+  margin: 15px 0;
   user-select: none;
+  text-align: right;
 }
 
 label {
-  cursor: pointer;
   font-size: 0;
 }
+
 span {
   display: inline-block;
   vertical-align: middle;
   font-size: 14px;
   font-weight: 500;
   line-height: 30px;
-  padding-right: 8px;
+  padding-left: 5px;
+  color: rgb(32, 82, 132);
+  transition: color 0.3s ease-in-out;
 }
+
 input {
   cursor: pointer;
   display: inline-block;
   vertical-align: middle;
   height: 18px;
   width: 18px;
-  border: 1px solid rgba(255, 255, 255, 0.15);
-  border-radius: 3px;
+  border: 1px solid rgb(198, 208, 235);
+  border-radius: 50%;
   background: rgba(255, 255, 255, 0.15);
   appearance: none;
   outline: 0;
+  transition: all 0.3s ease-in-out;
+  overflow: hidden;
 }
 input.selected:after {
-  content: '✓';
+  content: '';
   display: block;
   position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  color: #fff;
-  font-size: 15px;
+  top: 2px;
+  right: 2px;
+  bottom: 2px;
+  left: 2px;
+  background: rgb(156, 114, 248);
+  border-radius: 50%;
+  font-size: 14px;
   font-weight: 500;
   line-height: 16px;
   text-align: center;
+}
+
+/* 黑暗模式 */
+div.toggle.dark span {
+  color: rgb(198, 208, 235);
+}
+
+div.toggle.dark input {
+  border-color: rgba(255, 255, 255, 0.15);
 }
 </style>
